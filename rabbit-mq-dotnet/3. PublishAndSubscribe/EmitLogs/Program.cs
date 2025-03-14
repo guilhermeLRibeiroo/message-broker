cca5033cee37d57bae652c49f8d338d492829f08
DESKTOP-BI6AAGD\guilh <guilazribeiro@gmail.com>
2025-03-14 12:06:12 -0300

    Upgrade from rabbit-mq:3 to rabbit-mq:4 image and added exchange example

---
+++ rabbit-mq-dotnet/3. PublishAndSubscribe/EmitLogs/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "logs", routingKey: string.Empty, 
+    ody: body);
+Console.WriteLine($"Sent {message}");
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}
